390c1dd01bded657f1b0acceb4b28de1987a807d
sinem <siinem@gmail.com>
2023-06-26 15:17:00 +0200

    Inpainting code and explanations are added

---
+++ Inpainting/paths.py
@@ -0,0 +1,20 @@
+
+
+
+# original input images after exporting from segments.ai and merging three repositories into single file for segments_masks and segments_images
+segments_images_path = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/segments_images'
+segments_masks_path = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/segments_masks'
+
+# foreground, images and masks folder  after processing the segments_images and segments_masks
+fg_folder = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/fg/'
+images_folder = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/images/'
+masks_folder = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/masks/'
+
+# folders for cropped stuff:
+cropped_masks = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/masks_cropped/'
+cropped_images = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/images_cropped/'
+cropped_fgs = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/fg_cropped/'
+
+# black mark regions:
+bm_imgs = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/black_mark_region/'
+bm_cropped_imgs = '/home/sinem/PycharmProjects/fragment-restoration/Dataset/black_mark_region_cropped/'
